e2646bf80bfe8466a5d8c71017bb04ec9386dc40
daniel-efrat <daniel989@gmail.com>
2023-06-06 11:27:04 +0200

    font

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch("https://codex-0kvk.onrender.com", {
+  const response = await fetch("http://localhost:5000", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -93,6 +93,15 @@ const handleSubmit = async (e) => {
       prompt: data.get("prompt"),
     }),
   })
+  // const response = await fetch("https://codex-0kvk.onrender.com", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     prompt: data.get("prompt"),
+  //   }),
+  // })
 
   clearInterval(loadInterval)
   messageDiv.innerHTML = " "
